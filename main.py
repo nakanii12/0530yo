6821ac3c05976980844a62a9c9f8edc3b8fcf5a7
nakanii12 <101370415+nakanii12@users.noreply.github.com>
2022-05-30 11:41:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def search():
 def set_cokie():
     q = request.args.get("q")
     response = make_response("<head><title>連番を簡単に生成！！</title></head><body>"+" ".join([str(i) for i in range(int(q))])+"</body>")
-    if q == "85175":
+    if q == "090328":
         response.set_cookie("yuki",value="True")
     return response
 
